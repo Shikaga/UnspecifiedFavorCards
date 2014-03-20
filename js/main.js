aee5248f47be6c21230a9f35fac824f7f966a6cc
Shikaga <shikaga@gmail.com>
2014-03-20 19:39:14 +0100

    Fixing new keys

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ this.favorArray = ko.observableArray([
         "Unspecified Favor 2", null, null, false),
     new Favor("3fb0e6d1d27ee4734892dc1dc7c58719e1f28b8f45d1cbec1c8af1b1ab18bdf849aca3e2637db188ec9ad958f8cbbe454fb652e18f77023d15c449dd89ef0ccc",
         "Unspecified Favor 3", null, null, false),
-    new Favor("3fb0e6d1d27ee4734892dc1dc7c58719e1f28b8f45d1cbec1c8af1b1ab18bdf849aca3e2637db188ec9ad958f8cbbe454fb652e18f77023d15c449dd89ef0ccc",
+    new Favor("2eacffa23839e17b775abd8afcfabaa4ea95bdd208096a58c5bdb9bbae3771ed7b7d474e23fa2b77398e8ef20b05a6bb109a8ee64ccb33e7ab6715c7381755c0",
             "Unspecified Favor 4 - Deactivated until 2030", null, null, false),
-    new Favor("3fb0e6d1d27ee4734892dc1dc7c58719e1f28b8f45d1cbec1c8af1b1ab18bdf849aca3e2637db188ec9ad958f8cbbe454fb652e18f77023d15c449dd89ef0ccc",
+    new Favor("b3a82caa820fd138d9e41b83e5e4d0b5a65bb3044fd74b9c74a8a0a86b534cd77aa1be11b1d1dfd626d1db3dee37747b92811364b511648df8337f2a99235e56",
                 "Unspecified Favor 5 - Deactivated until 2032", null, null, false),
     new Favor("1e7cc2c503dbfc13255924dde56f0ec99abe8449142b74b44a0a32bf285261afb72d088c34de64f1f59e5398644e7060bfc342d49d4a60441020d478ec189010",
         "Test Favor", null, null, false)
